a105f71ecc7d70db01b042425ea24b044e682558
Adam Elfsborg <adamelfsborg.dev@gmail.com>
2024-07-09 18:00:28 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Adam Elfsborg on 2024-07-09.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
